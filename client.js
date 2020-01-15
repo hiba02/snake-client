5b0ba73c64dd3c7254f5d9cbd39b7474ca440ba6
hiba02 <kangyoonsoon@hotmail.com>
2020-01-15 20:14:09 +0000

    clinet.js inital name

--- client.js
+++ client.js
@@ -1,14 +1,20 @@
 const net = require('net');
 
+//The "connect" event is triggered on a connection as soon as it is successfully established.
 const connect = function() {
   const conn = net.createConnection({ 
     host: '192.168.0.103',
     port: 50541
   });
-
+  console.log('Sucess!');
   conn.setEncoding('utf8'); 
+  // message from Sever
   conn.on('data', (data) => {
     console.log('Meessage: ', data);
+  });
+  // client sending message to server
+  conn.on('connect', () => {
+    conn.write('Name: KYS');
   });
   return conn;
 }
