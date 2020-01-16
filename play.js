4790c410466d7321bc23a74725af2efaff085173
hiba02 <kangyoonsoon@hotmail.com>
2020-01-16 00:13:57 +0000

    Stdin Setup

--- play.js
+++ play.js
@@ -1,6 +1,18 @@
-const net = require('net');
-const  connect  = require('./client');
-console.log('Connecting ...');
-connect();
-// ?? it did not work with ES6
-// const { connect } = require('./client');
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  
+  stdin.on('data', handleUserInput)
+  return stdin;
+}
+
+setupInput();
+
